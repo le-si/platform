02c999c2984f43efa1397770511622b45b0fc2ad
Todd <todd.elvers@gmail.com>
2023-06-29 10:29:01 -0500

    Documentation pages have a tighter max width

--- packages/app/src/Markdown/Page/index.tsx
+++ packages/app/src/Markdown/Page/index.tsx
@@ -4,7 +4,7 @@ import { Markdown } from "~/Markdown";
 export function Page({ children }: React.PropsWithChildren) {
   return (
     <App.Page>
-      <div className="mx-auto max-w-[120rem]">
+      <div className="mx-auto max-w-[100rem]">
         {typeof children === "string" ? (
           <Markdown>{children}</Markdown>
         ) : (
