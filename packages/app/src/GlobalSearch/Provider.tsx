feca08206499c033170140a43ebc567f4825b363
Todd <todd.elvers@gmail.com>
2023-07-05 12:07:42 -0500

    Added a global search modal button to the topbar

--- packages/app/src/GlobalSearch/Provider.tsx
+++ packages/app/src/GlobalSearch/Provider.tsx
@@ -5,12 +5,12 @@ import { GlobalSearch } from ".";
 import { Modal as GlobalSearchModal } from "./Modal";
 
 export function Provider({ children }: React.PropsWithChildren) {
-  const { isOpen, setIsOpen } = GlobalSearch.Modal.useState();
+  const { open } = GlobalSearch.Modal.useState();
 
   React.useMemo(() => GlobalSearch.Engine.init(), []);
 
   /** @see https://react-hotkeys-hook.vercel.app */
-  useHotkeys(["meta+k", "ctrl+k"], () => setIsOpen(!isOpen), {
+  useHotkeys(["meta+k", "ctrl+k"], () => open(), {
     enableOnFormTags: ["INPUT", "TEXTAREA"],
     preventDefault: ({ key, ctrlKey, metaKey }) =>
       key === "k" && (ctrlKey || metaKey),
