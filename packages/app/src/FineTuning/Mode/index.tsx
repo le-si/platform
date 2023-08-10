1948f9f0775233a2921dfabd26eca54ac1dce546
kaj <40004347+KAJdev@users.noreply.github.com>
2023-08-10 09:25:39 -0800

    disable mode selection

--- packages/app/src/FineTuning/Mode/index.tsx
+++ packages/app/src/FineTuning/Mode/index.tsx
@@ -35,9 +35,8 @@ export function Mode({
 }) {
   const isModeSelected = Mode.use() === mode;
   const onClick = useCallback(() => {
-    Mode.set(mode);
     FineTuning.Steps.next();
-  }, [mode]);
+  }, []);
 
   return (
     <FineTuning.Card className="flex grow basis-0 flex-col gap-4">
