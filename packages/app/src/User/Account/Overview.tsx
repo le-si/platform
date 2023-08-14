6b21957cfe9589c29bb1806271fff5352e179f0f
kaj <40004347+KAJdev@users.noreply.github.com>
2023-08-14 15:19:11 -0800

    feat: display org ID on account page

--- packages/app/src/User/Account/Overview.tsx
+++ packages/app/src/User/Account/Overview.tsx
@@ -33,18 +33,25 @@ function UserDetails() {
 
   return (
     <div className="flex items-center gap-4">
-      <User.Avatar className="h-[100px] w-[100px]" />
-      <div className="flex flex-col justify-between gap-1 text-left">
+      <User.Avatar className="h-[75px] w-[75px]" />
+      <div className="flex flex-col justify-between gap-2 text-left">
         {user?.name ? (
-          <h5 className="text-2xl font-medium text-zinc-900 dark:text-white">
+          <h5 className="flex flex-col items-start gap-1 text-left text-3xl font-medium text-zinc-900 dark:text-white lg:flex-row lg:items-center">
             {user.name}
+            {user?.email ? (
+              <span className="text-left text-xl text-zinc-500 dark:text-zinc-400">
+                ({user.email})
+              </span>
+            ) : (
+              <Theme.Skeleton className="h-4 w-20" />
+            )}
           </h5>
         ) : (
           <Theme.Skeleton className="h-4 w-32" />
         )}
-        {user?.email ? (
-          <span className="text-md text-zinc-500 dark:text-zinc-400">
-            {user.email}
+        {user?.organizationID ? (
+          <span className="w-fit rounded border border-zinc-300 bg-black/10 px-1 text-sm text-zinc-600 dark:text-zinc-400">
+            {user.organizationID}
           </span>
         ) : (
           <Theme.Skeleton className="mt-2 h-4 w-20" />
